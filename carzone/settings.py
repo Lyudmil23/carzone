ee8657c86edff157ff38d1625fa0dee5118efeda
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-12 12:12:55 +0200

    Added files

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "carzone1_db",
+        "USER": "postgres",
+        "PASSWORD": "1123QwER",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
