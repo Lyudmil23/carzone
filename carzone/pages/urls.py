9eeeed8cd0a0cdde4a34ba0d40394142bded1cd7
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-14 10:12:35 +0200

    Added files

--- carzone/pages/urls.py
+++ carzone/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from carzone.pages.views import home
+from carzone.pages.views import home, about, services, contact
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('services/', services, name='services'),
+    path('contact/', contact, name='contact'),
 ]
