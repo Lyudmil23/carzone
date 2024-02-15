5a4f44b04d5e65c33005437bf17c284fa64c5e68
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-15 10:35:04 +0200

    Added files

--- carzone/pages/models.py
+++ carzone/pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+    designation = models.CharField(
+        max_length=100,
+    )
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField()
+    twitter_link = models.URLField()
+    google_plus_link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
