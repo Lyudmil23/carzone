73863603f31c93ebff13301004a9e3f05ccbbe84
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-21 12:15:07 +0200

    Added files

--- carzone/pages/views.py
+++ carzone/pages/views.py
@@ -7,9 +7,11 @@ from carzone.pages.models import Team
 def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-created_date')
     context = {
         'teams': teams,
         'featured_cars': featured_cars,
+        'all_cars': all_cars,
     }
     return render(request, 'pages/home.html', context)
 
