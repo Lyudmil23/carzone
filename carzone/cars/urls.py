632974286f6a7878940251f3f603ba1c0e178fd7
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-02-23 09:53:31 +0200

    Added files

--- carzone/cars/urls.py
+++ carzone/cars/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from carzone.cars.views import cars
+from carzone.cars.views import cars, car_details
 
 urlpatterns = [
     path('', cars, name='cars'),
+    path('<int:id>/', car_details, name='car_details')
 
 ]
--- carzone/cars/views.py
+++ carzone/cars/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def cars(request):
     return render(request, 'cars/cars.html')
+
+
+def car_details(request, id):
+    return render(request, 'cars/car-details.html')
