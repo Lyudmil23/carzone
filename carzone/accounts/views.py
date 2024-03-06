ba65f0e80fa608e276ff99ba41657925caa8f248
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-06 21:53:23 +0200

    Added files

--- carzone/accounts/views.py
+++ carzone/accounts/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render, redirect
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
