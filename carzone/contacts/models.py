b352ab65586b98c1b426c03c5f4fa9c1b825d08a
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-26 12:40:19 +0200

    Added files

--- carzone/contacts/models.py
+++ carzone/contacts/models.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(
+        max_length=50,
+    )
+    car_id = models.IntegerField()
+    customer_need = models.CharField(
+        max_length=50,
+    )
+    car_title = models.CharField(
+        max_length=50,
+    )
+    city = models.CharField(
+        max_length=50,
+    )
+    state = models.CharField(
+        max_length=50,
+    )
+    email = models.EmailField()
+    phone = models.CharField(
+        max_length=50,
+    )
+    message = models.TextField(
+        blank=True,
+    )
+    user_id = models.IntegerField(
+        blank=True,
+    )
+    create_date = models.DateTimeField(
+        blank=True,
+        default=datetime.now(),
+    )
+
+    def __str__(self):
+        return self.email
