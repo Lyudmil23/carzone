d961fcbab3e1e3c73d31f3aeb09fce33b6a7005a
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-31 18:48:02 +0300

    Added files

--- carzone/settings.py
+++ carzone/settings.py
@@ -155,6 +155,12 @@ MESSAGE_TAGS = {
 
 SITE_ID = 1
 
+#Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lyudmil@abv.bg'
+EMAIL_HOST_PASSWORD = '12345'
+EMAIL_USE_TLS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
